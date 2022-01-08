5423090b452415e630db979a10b1265568b3a88c
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-01-09 03:24:31 +0900

    Updates

---
+++ precache-manifest.f5792b58dc831bbbd2fcdce298425691.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3982be93fb567b7952399ba8a2518b43",
+    "url": "/react-for-beginners/index.html"
+  },
+  {
+    "revision": "041429344cf1a955183b",
+    "url": "/react-for-beginners/static/css/main.9facb8c6.chunk.css"
+  },
+  {
+    "revision": "3a803bc74bdf45c960a3",
+    "url": "/react-for-beginners/static/js/2.72969a32.chunk.js"
+  },
+  {
+    "revision": "041429344cf1a955183b",
+    "url": "/react-for-beginners/static/js/main.0f20574c.chunk.js"
+  },
+  {
+    "revision": "6873a68efa9d37e96a89",
+    "url": "/react-for-beginners/static/js/runtime~main.08ccc910.js"
+  }
+]);
